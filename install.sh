e3fce14f4b4575c900981a57e8e402601d85b984
Mark Otto <markd.otto@gmail.com>
2021-07-27 15:01:15 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+
+# Symlink dotfiles
+# ln -s $(pwd)/bin "$HOME/bin"
+ln -s $(pwd)/.gitconfig "$HOME/.gitconfig"
+ln -s $(pwd)/.gitignore "$HOME/.gitignore"
+ln -s $(pwd)/.bash_profile "$HOME/.bash_profile"
